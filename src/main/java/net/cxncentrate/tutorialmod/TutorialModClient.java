44378966767fe01ccb83afd40199611d186500b0
Niegoo11 <nicolas.diewald@gmail.com>
2024-05-07 19:10:23 +0200

    Initial commit

---
+++ src/main/java/net/cxncentrate/tutorialmod/TutorialModClient.java
@@ -0,0 +1,10 @@
+package net.cxncentrate.tutorialmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TutorialModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
