9457eff3aa9108fe9be97da02e4aad40cb93c9ed
Niegoo11 <nicolas.diewald@gmail.com>
2024-05-14 18:58:45 +0200

    Ruby Block & Items

--- src/main/java/net/cxncentrate/tutorialmod/TutorialMod.java
+++ src/main/java/net/cxncentrate/tutorialmod/TutorialMod.java
@@ -1,5 +1,8 @@
 package net.cxncentrate.tutorialmod;
 
+import net.cxncentrate.tutorialmod.block.ModBlocks;
+import net.cxncentrate.tutorialmod.item.ModItemGroups;
+import net.cxncentrate.tutorialmod.item.ModItems;
 import net.fabricmc.api.ModInitializer;
 
 import org.slf4j.Logger;
@@ -11,6 +14,9 @@ public class TutorialMod implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
-		LOGGER.info("Hello Fabric world!");
+		ModItemGroups.registerItemGroups();
+
+		ModItems.registerModItems();
+		ModBlocks.registerModBlocks();
 	}
 }
